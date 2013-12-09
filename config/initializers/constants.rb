14ea94801c392d40015064288028aee57be3da46
jon <goodeyjon@hotmail.com>
2013-12-09 14:48:38 +0000

    changes to the site url

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
-APP_ID= '661800997197519' # please change!
-APP_SECRET= 'e7addf478dd5103685a84983de864c96' # please change!
-SITE_URL = 'http://localhost:3000'
+APP_ID = '661800997197519' # please change!
+APP_SECRET = 'e7addf478dd5103685a84983de864c96' # please change!
+SITE_URL = 'http://koalatestjon.herokuapp.com'
